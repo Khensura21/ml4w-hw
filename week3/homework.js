dc044d8aa31122e37466a860cc6066905d4dac96
Khensu-Ra Love El <khensura25@10-18-159-55.dynapool.wireless.nyu.edu>
2019-04-16 12:41:08 -0400

    Homework 3 JS file

---
+++ week3/homework.js
@@ -0,0 +1,96 @@
+
+{/* 
+<script type="text/javascript">
+    const checkpoint = 'https://storage.googleapis.com/tm-pro-a6966.appspot.com/2019-04-16-11-46-18/model.json';
+
+    let model;
+    const maxPredictions = 4;
+    async function load() {
+        model = await tm.mobilenet.load(checkpoint);
+    }
+
+    async function init() {
+        await load();
+        const prediction = await model.predict(image, maxPredictions);
+    }
+
+    init();
+</script> */}
+
+
+
+
+
+
+const checkpoint = 'https://storage.googleapis.com/tm-pro-a6966.appspot.com/Yining-image-example/model.json';
+const maxPredictions = 4;
+
+let model;
+let video;
+let water;
+// A function that loads the model
+async function load() {
+  model = await tm.mobilenet.load(checkpoint);
+}
+
+async function setup() {
+  // Call the load function, wait until it finishes loading
+  await load();
+
+  // Get videos from webcam
+  video = createCapture(VIDEO);
+  video.size(400, 300);
+
+  // Make a prediction on video
+  predictVideo(video.elt);
+
+
+  //create a canvas 
+  createCanvas(500, 500);
+}
+
+
+async function predictVideo(image) {
+  const prediction = await model.predict(image, maxPredictions);
+  console.log('prediction: ', prediction)
+
+  // Show the result
+   const res = select('#res'); // select <span id="res">
+   res.html(prediction[0].className);
+
+  if (res.html(prediction[0].className) == 1){
+    console.log("hi");
+    waterViz();
+} else{
+   text('Where is the Food Brooo?', 120, 75)
+}
+
+  // Show the probability
+  const prob = select('#prob'); // select <span id="prob">
+  prob.html(prediction[0].probability);
+
+  // Continue to predict the video
+  predictVideo(video.elt);
+}
+
+function waterViz() {
+    background(49, 153, 227)
+  
+    textStyle(BOLD);
+    textFont('HELVETICA', 25);
+    text('What Are The Macros?', 120, 75)
+    textFont('HELVETICA', 20);
+  
+    textStyle(ITALIC);
+    text('Item: Water', 210, 105);
+  
+    textStyle(NORMAL);
+    textFont('HELVETICA', 21);
+    text('Protein: 0', 50, 170)
+    text('Carbs: 0', 50, 240)
+    text('Fats: 0', 50, 310)
+  
+  }
+// function draw(){
+    
+// }
